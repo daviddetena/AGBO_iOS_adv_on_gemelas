198208ce718efbd756cdd729e4934609b914bade
David de Tena <daviddetenacalvo@gmail.com>
2015-04-06 13:36:19 +0200

    Aynchronous download of the image in a queue and set to UIImageView within a block executing in the main queue

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 This project shows how to manage background connections with GCD.
 
 1. Synchronous download of an image to show the UI gets blocked.
+2. Change code for synchronous download to async. We download the image in background in another queue and right after that we execute the code that set the downloaded image to UIImageView in the main queue.
--- gemelas/DTCImageViewController.m
+++ gemelas/DTCImageViewController.m
@@ -26,10 +26,29 @@
 
 
 - (IBAction)downloadImage:(id)sender {
-    NSURL *url = [NSURL URLWithString:@"http://media.vandal.net/master/14199/20121210152529_3.jpg"];
-    NSData *imgData = [NSData dataWithContentsOfURL:url];
+
+    // Create a queue
+    dispatch_queue_t queue = dispatch_queue_create("GOW", 0);
     
-    UIImage *image = [UIImage imageWithData:imgData];
-    self.photoView.image = image;
+    // Send our custom queue a block to be executed in background.
+    // Blocks get a constant copy of the environment so objects can not be modified unless we define
+    // them with __block outside the block
+    __block UIImage *image = nil;
+    __block NSData *imgData = nil;
+
+    dispatch_async(queue, ^{
+        // Download
+        NSURL *url = [NSURL URLWithString:@"http://media.vandal.net/master/14199/20121210152529_3.jpg"];
+        imgData = [NSData dataWithContentsOfURL:url];
+        
+        // We need UIKit code to be run in foreground but right after getting imgData,
+        // so we execute this code in the main queue within a block
+        // Jump from our queue to the main queue
+        dispatch_async(dispatch_get_main_queue(), ^{
+            image = [UIImage imageWithData:imgData];
+            self.photoView.image = image;
+
+        });
+    });
 }
 @end
